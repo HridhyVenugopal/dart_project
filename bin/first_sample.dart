ae587039fa6a9e69ba5f69692dda35a193847c8c
HridhyVenugopal <hridhyvenugopal@gmail.com>
2023-09-18 12:50:17 +0530

    first commit

---
+++ bin/first_sample.dart
@@ -0,0 +1,6 @@
+void main(){
+   print("I am Hridhy Venugopal");
+   print(22);
+   print(0123456789);
+   print(6.1);
+}
---
+++ bin/new_file.dart
@@ -0,0 +1,9 @@
+void main(){
+  String name = "Hridhy Venugopal";
+  int age = 22;
+  int phno = 0123456789;
+  double cgpa = 6.01;
+  String email = "hridhyvenugopalan@gmail.com";
+  print("My name is " + name);
+  
+}
