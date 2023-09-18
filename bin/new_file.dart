8589ef393b00dd82c8da94c55884afa9fb1c65ce
HridhyVenugopal <hridhyvenugopal@gmail.com>
2023-09-18 12:54:02 +0530

    initial commit

--- bin/new_file.dart
+++ bin/new_file.dart
@@ -3,7 +3,10 @@ void main(){
   int age = 22;
   int phno = 0123456789;
   double cgpa = 6.01;
-  String email = "hridhyvenugopalan@gmail.com";
-  print("My name is " + name);
-  
+  String email = "hridhyvenugopalan@gmail.com";;
+  print("My name is $name");
+  print("I am $age years old" );
+  print("My phone numner is $phno");
+  print("My cgpa is $cgpa");
+  print("My email id is $email");
 }
